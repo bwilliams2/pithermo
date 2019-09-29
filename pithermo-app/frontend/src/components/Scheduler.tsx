71ceac0d866770661f2b39c0ee41ce49baa23dd0
Bryce Williams <bwilliams2@gmail.com>
2019-09-29 07:49:21 -0500

    Major updates

---
+++ pithermo-app/frontend/src/components/Scheduler.tsx
@@ -0,0 +1,22 @@
+import React, { useState, FunctionComponent } from "react";
+import Grid from "@material-ui/core/Grid";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+
+type SchedulerProps = {
+
+}
+
+const Scheduler: FunctionComponent<SchedulerProps> = props => {
+
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <FormControl>
+                </FormControl>
+            </Grid>
+        </Grid>
+    )
+}
